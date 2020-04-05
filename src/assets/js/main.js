4e540832895cb823f175a5ff76f9f1769b499e24
Vinicius Otacilio <viniotacilio@gmail.com>
2020-04-05 00:50:05 -0300

    Css completo

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,6 +7,20 @@ let features = [
     testHours: 2
   }
 ];
+// Implementação do modal aparecendo/desaparecendo
 
+let modal = document.getElementById("insert-modal");
 
+let openModal = document.getElementById("open-insert-modal");
+
+let closeModal = document.getElementById("close-modal");
+
+openModal.onclick = function () {
+  modal.style.display = "flex";
+}
+
+closeModal.onclick = function (event) {
+    modal.style.display = "none";
+  
+}
 // Dica: faça o layout e depois pense em como vai funcionar o script.
