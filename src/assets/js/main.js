1ded93d35257f5492638e41a891f87be4e0f6295
Vinicius Otacilio <viniotacilio@gmail.com>
2020-04-11 03:53:38 -0300

    Adicionando funcionalidade de importar .json, correção de bugs e melhorei a qualidade do código

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,3 @@
-// Este será o objeto principal no qual você irá se basear para as funções do desafio!
-// Caso haja dúvidas de como prosseguir, favor consultar a sala #js no nosso discord!
-let features = [
-  {
-    feature: "Authentication",
-    devHours: 10,
-    testHours: 2
-  }
-];
-
 // Implementação do modal aparecendo/desaparecendo
 
 let modal = document.getElementById("insert-modal");
@@ -34,6 +24,7 @@ let valueHoursCost = 0;
 valueHour.addEventListener("change", (event) => {
   event.preventDefault();
   valueHoursCost = valueHour.value;
+  console.log(valueHoursCost);
 })
 
 const inputFunc = document.getElementById('insert-functionalities');
@@ -43,34 +34,42 @@ inputFunc.addEventListener("click", (event) => {
     let nameFunc = document.getElementById('name-functionalities').value;
     let developHours = document.getElementById('develop-hours').value;
     let testHours = document.getElementById('test-hours').value;
-    modal.style.display=  "none";
-
-    //Criando DIV com os valores
-    let div = document.createElement('DIV');
-    div.classList.add('functionalities-input');
-    div.id = 'functionalities-input';
-    let functionalityName = document.createElement('P');
-    let textFunctionality = document.createTextNode(nameFunc);
-    let HourDevelopeds = document.createElement('P');
-    let textDevel = document.createTextNode(developHours);
-    let HourText = document.createElement('P');
-    let testHour = document.createTextNode(testHours);
-    let ValueText = document.createElement('P');
-    let ValueFunctionality = document.createTextNode(valueHoursCost);
-
-    functionalityName.appendChild(textFunctionality);
-    HourDevelopeds.appendChild(textDevel);
-    HourText.appendChild(testHour);
-    ValueText.appendChild(ValueFunctionality);
-
-    div.appendChild(functionalityName);
-    div.appendChild(HourDevelopeds);
-    div.appendChild(HourText);
-    div.appendChild(ValueText);
-
-    document.getElementById('functionalities-box').appendChild(div);
+    modal.style.display = "none";
+    adicionarFuncionalidades(nameFunc,developHours,testHours);
 });
 
+function adicionarFuncionalidades(nameFunc,DevelopHours,TestHours) {
+
+  let DevelopHoursToNumber = parseInt(DevelopHours,10);
+  let TestHoursToNumber = parseInt(TestHours,10);
+
+  let featureValueHour = valueHoursCost * (DevelopHoursToNumber + TestHoursToNumber); 
+
+  let div = document.createElement('DIV');
+  div.classList.add('functionalities-input');
+  div.id = 'functionalities-input';
+  let functionalityName = document.createElement('P');
+  let textFunctionality = document.createTextNode(nameFunc);
+  let HourDevelopeds = document.createElement('P');
+  let textDevel = document.createTextNode(DevelopHours);
+  let HourText = document.createElement('P');
+  let testHour = document.createTextNode(TestHours);
+  let ValueText = document.createElement('P');
+  let ValueFunctionality = document.createTextNode(featureValueHour);
+
+  functionalityName.appendChild(textFunctionality);
+  HourDevelopeds.appendChild(textDevel);
+  HourText.appendChild(testHour);
+  ValueText.appendChild(ValueFunctionality);
+
+  div.appendChild(functionalityName);
+  div.appendChild(HourDevelopeds);
+  div.appendChild(HourText);
+  div.appendChild(ValueText);
+
+  document.getElementById('functionalities-box').appendChild(div);
+
+};
 // Fim da implementação de inputs em variáveis;
 
 // Remover features
@@ -100,4 +99,36 @@ function sendFile () {
   document.getElementById('fileid').click();
 }
 
+let objectJson;
+document.getElementById('fileid').addEventListener('change',handleFile);
+
+function handleFile(e) {
+    let files = e.target.files;
+    f = files[0];
+    let reader = new FileReader();
+
+    reader.onload = (function(file) {
+        return function(e) {
+          const necessaryProperties = ["feature","devHours","testHours"];
+          JsonObj = JSON.parse(e.target.result);
+          objectJson = JsonObj;
+          objectJson.forEach(function(object) {
+          let hasAll = necessaryProperties.every(necessaryProperties => object.hasOwnProperty(necessaryProperties));
+            if(hasAll == false){
+              alert('Insira um JSON válido');
+            }
+            else {
+              adicionarFuncionalidades(object.feature, object.devHours, object.testHours);
+            }
+          })
+
+        };
+      })(f);
+      reader.readAsText(f);
+}
+
+
+
+
+// BUG - Não consigo enviar outro arquivo JSON após o primeiro sem ter que reiniciar a pagina (Não resolvido)
 // BUG - Se o usuário não escrever nada na hora de adicionar funcionalidade, o campo é criado todo em branco. (Não resolvido)
