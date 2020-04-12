330d3440bf84315b77e4ad1d6528441b85344e7e
Vinicius Otacilio <viniotacilio@gmail.com>
2020-04-12 06:02:38 -0300

    Arrumei funçãod de remover e adicionei solução para encontrar os valores totais, falta apenas mostrar no DOM

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,5 @@
 let feature = [];
+let ValorTotal = 0;
 // Implementação do modal aparecendo/desaparecendo
 let modal = document.getElementById("insert-modal");
 
@@ -24,7 +25,6 @@ let valueHoursCost = 0;
 valueHour.addEventListener("change", (event) => {
   event.preventDefault();
   valueHoursCost = valueHour.value;
-  console.log(valueHoursCost);
 })
 
 const inputFunc = document.getElementById('insert-functionalities');
@@ -50,7 +50,7 @@ function adicionarFuncionalidades(nameFunc,DevelopHours,TestHours) {
   let TestHoursToNumber = parseInt(TestHours,10);
 
   let featureValueHour = valueHoursCost * (DevelopHoursToNumber + TestHoursToNumber); 
-
+  ValorTotal = ValorTotal + featureValueHour;
   let div = document.createElement('DIV');
   div.classList.add('functionalities-input');
   div.id = 'functionalities-input';
@@ -74,7 +74,7 @@ function adicionarFuncionalidades(nameFunc,DevelopHours,TestHours) {
   div.appendChild(ValueText);
 
   document.getElementById('functionalities-box').appendChild(div);
-
+  resumo();
 };
 // Fim da implementação de inputs em variáveis;
 
@@ -83,10 +83,21 @@ function adicionarFuncionalidades(nameFunc,DevelopHours,TestHours) {
 const deleteInput = document.getElementById('delete-input');
 
 function removeInput (e) {
+  let removeFromObject;
   e = e || window.event;
   let target = e.target || e.srcElement;
   let divInput = target.parentNode;
   if(divInput.id === 'functionalities-input') {
+    removeFromObject = divInput.innerText.replace(/(\r\n|\n|\r)/gm," ").split(/ (.*)/);
+    for (let i = 0; i < feature.length; i++){
+      if(feature[i].feature == removeFromObject[0]){
+        feature.splice(i,1);
+        break;
+      }
+      else {
+        alert('Algo deu errado');
+      }
+    };
     divInput.remove(0);
     document.removeEventListener('click', removeInput);
   }
@@ -105,10 +116,10 @@ function sendFile () {
   document.getElementById('fileid').click();
 }
 
-let objectJson;
 document.getElementById('fileid').addEventListener('change',handleFile);
 
 function handleFile(e) {
+    let objectJson;
     let files = e.target.files;
     f = files[0];
     let reader = new FileReader();
@@ -159,5 +170,22 @@ function ExportarFuncionalidades () {
 };
 
 // Fim de exportação de JSON;
+
+// Barra de Resumo (todas as funções)
+
+function resumo () {
+  let totalFuncionalidades = feature.length;
+  let totalHorasDesenvolvimento = 0;
+  let totalHorasTeste = 0;
+  // Loop para descobrir as quantidades de horas de desenvolvimento e de teste.
+  for(let i = 0; i < feature.length; i++){
+    let toNumber = parseInt(feature[i].devHours);
+    let toNumberTest = parseInt(feature[i].testHours);
+    totalHorasTeste = totalHorasTeste + toNumberTest;
+    totalHorasDesenvolvimento = totalHorasDesenvolvimento + toNumber;
+  }
+  console.log(totalHorasTeste,totalHorasDesenvolvimento,ValorTotal);
+}
+
 
 // BUG - Se o usuário não escrever nada na hora de adicionar funcionalidade, o campo é criado todo em branco. (Não resolvido)
